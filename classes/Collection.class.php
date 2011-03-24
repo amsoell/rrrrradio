b01b48b3719f5d3b5eea9e945123e9ef50796c46
Andy Soell <andy@teamsoell.com>
2011-03-24 08:48:59 -0400

    Update autoqueue to respect the  flag

--- classes/Collection.class.php
+++ classes/Collection.class.php
@@ -3,7 +3,7 @@
     function getRandomTrack() {
       $db = new Db();
 
-      $rs = $db->query("SELECT `key` FROM track WHERE rnd>RAND() ORDER BY rnd LIMIT 1");
+      $rs = $db->query("SELECT `key` FROM track WHERE rnd>RAND() AND requested=1 ORDER BY rnd LIMIT 1");
       if ($rec = mysql_fetch_array($rs)) {
         $t = new Track($rec['key']);
         return $t;
