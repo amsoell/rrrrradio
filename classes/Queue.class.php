e9fd24bf6cdfb00654cecfbfec14cf57ff946a1e
Andy Soell <andy@teamsoell.com>
2011-03-25 08:50:31 -0400

    Shortened statplay, endplay values by a second to fix sync issue

--- classes/Queue.class.php
+++ classes/Queue.class.php
@@ -34,7 +34,7 @@
       $track = new Track($key);
 
       if ($track->canStream) {
-        $db->query("INSERT INTO queue (trackKey, userKey, added, startplay, endplay) VALUES ('$key', ".(is_null($requestedBy)?"NULL":"'$requestedBy'").", UNIX_TIMESTAMP(NOW()), ".($endplay+1).", ".($endplay+$track->duration+1).")");
+        $db->query("INSERT INTO queue (trackKey, userKey, added, startplay, endplay) VALUES ('$key', ".(is_null($requestedBy)?"NULL":"'$requestedBy'").", UNIX_TIMESTAMP(NOW()), ".($endplay).", ".($endplay+$track->duration).")");
       }
       
       if ($requested) $db->query("UPDATE track SET requested=1 WHERE `key`='$key' AND requested=0");
