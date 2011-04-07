012ebc960fca7e88f783b7175a00b13c80a84525
Andy Soell <andy@teamsoell.com>
2011-04-07 11:07:33 -0400

    Added Google Analytics tracking code

--- include/footer.php
+++ include/footer.php
@@ -8,5 +8,18 @@
         rrrrradio is powered by <a href="<?php print $c->affiliate_link_subscribe; ?>" target="_blank"><img src="/theme/cramppbo/images/RdioWhiteLogo.png" border="0" /></a>
       </div>
     </div>
+    <script type="text/javascript">
+    
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '<?php print $c->ga_tag; ?>']);
+      _gaq.push(['_trackPageview']);
+    
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    
+    </script>    
   </body>
 </html>
