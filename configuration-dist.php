7230b6de2451bb3b06ed11f154585817f366ccd1
Andy Soell <andy@teamsoell.com>
2011-05-11 14:03:15 -0400

    Updates to configuration format

--- configuration-dist.php
+++ configuration-dist.php
@@ -3,13 +3,12 @@
   define('RDIO_CONSSEC', 'GrP5WnY7gF');
 
   class Config {
-    var $sitename = "crumppbo";
+    var $sitename = "rrrrradio";
     var $app_domain = "domainname.com";
     var $app_dir = "/";  
     var $rdio_callback_url = "";
     var $rdio_oembed_url = "http://www.rdio.com/api/oembed/";
-    var $rdio_activeplaylist_url = "http://rd.io/x/QVvMyzNeAas";
-    var $rdio_collection_userkey = "userKey";
+    var $rdio_collection_userkey = "s298644";
     
     var $admin_email = "your@email.com";
     
@@ -25,12 +24,22 @@
     var $theme = "cramppbo";
     
     var $affiliate_link_subscribe = "http://click.linksynergy.com/fs-bin/click?id=Y5hfCBRENkU&offerid=221756&type=3&subid=0";
+    var $affiliate_link_prefix = "http://click.linksynergy.com/fs-bin/click?id=Y5hfCBRENkU&subid=&offerid=221756.1&type=10&tmpid=7950&RD_PARM1=";
     var $ga_tag = "";
     
     var $free_if_queue_less_than = 7;
     var $requests_per_hour = 5;
     var $requests_per_artist_per_hour = 3;
     var $requests_per_album_per_hour = 2;
+    
+    // HOURS BEFORE REPEATING RANDOMLY QUEUED TRACKS
+    var $random_rotation = 5;
+    // MAXIMUM LENGTH (IN SECONDS) FOR RANDOMLY QUEUED TRACKS
+    var $random_max_length = 300;
+    
+    var $autoplay = true;
+    var $refresh_interval = 5;
+    var $debug = false;
     
     function __construct() {
       $this->rdio_callback_url = "http://".$this->app_domain.$this->app_dir;
